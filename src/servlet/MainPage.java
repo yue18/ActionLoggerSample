0b95a40883fd57b7fc9e2ee5cb143bdc936a11c4
CelestLeaf <52768318+CelestLeaf@users.noreply.github.com>
2020-09-15 11:20:40 +0900

    add:Page

--- src/servlet/MainPage.java
+++ src/servlet/MainPage.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MainPage
@@ -16,29 +17,42 @@ import javax.servlet.http.HttpServletResponse;
 public class MainPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public MainPage() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	    // �t�H���[�h
-	    RequestDispatcher dispatcher =
-	        request.getRequestDispatcher
-	            ("/WEB-INF/jsp/mainView.jsp");
-	    dispatcher.forward(request, response);
+	public MainPage() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// HttpSessionインタフェースのオブジェクトを取得
+		HttpSession session = request.getSession();
+		// useridデータをsessionスコープで保存
+		String userid = (String) session.getAttribute("userid");
+
+		if (userid == null) {
+			// MainViewを表示
+			response.sendRedirect("/ActionLoggerSample/login");
+
+		} else {
+			// MainViewを表示
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mainView.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
